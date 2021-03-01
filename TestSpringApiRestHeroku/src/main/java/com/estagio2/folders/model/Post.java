099f62d0a46829f41f8057e105e484eb0e758404
joaopfds <joao.sousa2811@gmail.com>
2021-03-01 17:06:32 -0300

    new

--- TestSpringApiRestHeroku/src/main/java/com/estagio2/folders/model/Post.java
+++ TestSpringApiRestHeroku/src/main/java/com/estagio2/folders/model/Post.java
@@ -32,7 +32,7 @@ public class Post {
 	
 	
 	@OneToMany(mappedBy = "post")
-	@JsonIgnoreProperties("post")
+	//@JsonIgnoreProperties("post")
 	private List<Comentario> comentarios = new ArrayList<>();
 
 	public Long getId() {
