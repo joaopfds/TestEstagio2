d916444d94bf4e8728a0c767830f6b7b65fd91b7
joaopfds <joao.sousa2811@gmail.com>
2021-03-28 15:29:09 -0300

    atualizando

--- TestSpringApiRestHeroku/src/main/java/com/estagio2/folders/resource/GeralController.java
+++ TestSpringApiRestHeroku/src/main/java/com/estagio2/folders/resource/GeralController.java
@@ -3,8 +3,6 @@ package com.estagio2.folders.resource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import ch.qos.logback.core.net.LoginAuthenticator;
-
 @RestController
 public class GeralController {
 	
