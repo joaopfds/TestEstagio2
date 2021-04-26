bc4875b993a26ca9d5741cb728e2d08deee50b68
joaopfds <joao.sousa2811@gmail.com>
2021-04-25 21:47:47 -0300

    atualizando

--- TestSpringApiRestHeroku/src/main/java/com/estagio2/folders/repository/Usuarios.java
+++
@@ -1,9 +0,0 @@
-package com.estagio2.folders.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.estagio2.folders.model.Usuario;
-
-public interface Usuarios extends JpaRepository<Usuario, Long> {
-
-}
