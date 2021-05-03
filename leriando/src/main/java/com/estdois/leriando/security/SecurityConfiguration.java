08fb3a04936df8f7c64f9a75266dac426a815c99
João Pedro Fructuoso <54864399+joaopfds@users.noreply.github.com>
2021-05-02 21:59:31 -0300

    Update SecurityConfiguration.java

--- leriando/src/main/java/com/estdois/leriando/security/SecurityConfiguration.java
+++ leriando/src/main/java/com/estdois/leriando/security/SecurityConfiguration.java
@@ -37,7 +37,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/", "/lerind/**").permitAll()
+                .antMatchers("/", "/lerind/lerind/**").permitAll()
                 .antMatchers("/admin").access("hasAuthority('ADMIN')")
                 .anyRequest().authenticated()
                 .and().formLogin().loginPage("/login").permitAll()
