d136acbfe4a004a2bdc681e5888506a68be3f829
João Pedro Fructuoso <54864399+joaopfds@users.noreply.github.com>
2021-05-16 20:26:19 -0300

    Update and rename UserRepository.java to UsrRepository.java

--- proseando/src/main/java/com/estdois/proseando/repository/UsrRepository.java
+++ proseando/src/main/java/com/estdois/proseando/repository/UsrRepository.java
@@ -3,10 +3,10 @@ package com.estdois.proseando.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.estdois.proseando.entity.User;
+import com.estdois.proseando.entity.Usr;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends CrudRepository<Usr, Long> {
     //User findByNome (String nome);
 
 }
