2dff1eef093b3b6ca89489affa67ce8c20aa189e
João Pedro Fructuoso <54864399+joaopfds@users.noreply.github.com>
2021-05-23 19:27:25 -0300

    Add files via upload

---
+++ proseando/src/main/java/com/estdois/proseando/security/InMemorySecurityConfig.java
@@ -0,0 +1,17 @@
+package com.estdois.proseando.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+
+@Configuration
+public class InMemorySecurityConfig {
+	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder builder) throws Exception {
+		builder
+			.inMemoryAuthentication()
+			.withUser("adm").password("123");
+	}
+
+}
