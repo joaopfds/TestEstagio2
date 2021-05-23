934a2fdd8ff7b9cab44de551cf0827fdc2b5049f
João Pedro Fructuoso <54864399+joaopfds@users.noreply.github.com>
2021-05-23 15:36:04 -0300

    Delete TestSpringApiRestHeroku directory

--- TestSpringApiRestHeroku/src/main/java/com/estagio2/folders/repository/Comentarios.java
+++
@@ -1,9 +0,0 @@
-package com.estagio2.folders.repository; 
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.estagio2.folders.model.Comentario;
-
-public interface Comentarios extends JpaRepository<Comentario, Long> {
-
-}
--- TestSpringApiRestHeroku/src/main/java/com/estagio2/folders/repository/Posts.java
+++
@@ -1,9 +0,0 @@
-package com.estagio2.folders.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.estagio2.folders.model.Post;
-
-public interface Posts extends JpaRepository<Post, Long> {
-
-}
