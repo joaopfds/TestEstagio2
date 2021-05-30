245a3cef420743717a0bd0dde2748b5304fea12b
João Pedro Fructuoso <54864399+joaopfds@users.noreply.github.com>
2021-05-30 16:22:27 -0300

    Update SegurancaConfig.java

--- proseando/src/main/java/com/estdois/proseando/security/SegurancaConfig.java
+++ proseando/src/main/java/com/estdois/proseando/security/SegurancaConfig.java
@@ -15,7 +15,9 @@ public class SegurancaConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
-			.authorizeRequests().anyRequest().authenticated()
+			.authorizeRequests()
+				.anyRequest()	
+				.authenticated()
 			.and()
 			.formLogin()
 		;
